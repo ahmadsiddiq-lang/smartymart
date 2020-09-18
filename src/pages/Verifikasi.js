54e2b3846ed51a2632d9151c5b37f4719942a7d1
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-09-18 09:55:39 +0700

    strukture

---
+++ src/pages/Verifikasi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Verifikasi() {
+    return (
+        <View>
+            <Text>Verifikasi</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/routers/MainNavigation.js
@@ -0,0 +1,19 @@
+// In App.js in a new project
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../pages/Home';
+
+const Stack = createStackNavigator();
+
+function MainNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" headerMode="none">
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainNavigation;
