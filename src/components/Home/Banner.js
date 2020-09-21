661dec618f8a6844218aa6828bd98be5ad1300c1
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-09-21 10:36:32 +0700

    list product

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, ScrollView } from 'react-native';
+import { SCREEN_WIDTH, sizeHeight, sizeWidth } from '../../assets/responsive';
+import { bgBlack2, MainColor } from '../../assets/colors';
+
+export default class Banner extends Component {
+    scrollRef = React.createRef();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            indexOf: 0,
+        };
+    }
+
+    //setIndex
+    handleSetIndex = (e) => {
+        const viewSize = e.nativeEvent.layoutMeasurement.width;
+        const contentOffset = e.nativeEvent.contentOffset.x;
+        const selectedIndex = Math.floor(contentOffset / viewSize);
+        this.setState({
+            indexOf: selectedIndex,
+        });
+    };
+
+    componentDidMount = () => {
+        this.interval = setInterval(() => {
+            this.setState(prev => ({ indexOf: prev.indexOf === this.props.data.length - 1 ? 0 : prev.indexOf + 1 }),
+                () => {
+                    this.scrollRef.current.scrollTo({
+                        animatde: true,
+                        y: 0,
+                        x: SCREEN_WIDTH * this.state.indexOf,
+                    });
+                }
+            );
+        }, 3000);
+    }
+
+    componentWillUnmount() {
+        // Clear the interval right before component unmount
+        clearInterval(this.interval);
+    }
+    render() {
+        return (
+            <View style={styles.Container}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                    onMomentumScrollEnd={this.handleSetIndex}
+                    ref={this.scrollRef}
+                >
+                    {
+                        this.props.data.map((item, index) => {
+                            return (
+                                <View style={styles.BoxImage} key={index}>
+                                    <Image style={styles.Image} source={item.image} />
+                                </View>
+                            );
+                        })
+                    }
+                </ScrollView>
+                <View style={styles.BoxCircle}>
+                    {
+                        this.props.data.map((item, index) => {
+                            return (
+                                <View key={index} style={[styles.Circle, { backgroundColor: index === this.state.indexOf ? MainColor : bgBlack2 }]} />
+                            );
+                        })
+                    }
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    Container: {
+
+    },
+    BoxImage: {
+        width: SCREEN_WIDTH,
+        height: sizeHeight(24),
+    },
+    Image: {
+        resizeMode: 'contain',
+        width: SCREEN_WIDTH,
+        height: sizeHeight(24),
+    },
+    BoxCircle: {
+        position: 'absolute',
+        bottom: 15,
+        // borderWidth: 1,
+        width: SCREEN_WIDTH,
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    Circle: {
+        width: sizeWidth(2),
+        height: sizeWidth(2),
+        borderRadius: 20 / 2,
+        marginHorizontal: 3,
+    },
+});
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MainColor } from '../assets/colors';
+import { StyleSheet, View } from 'react-native';
+import { bgWhite } from '../assets/colors';
 import Header from '../components/Headers/HeaderHome';
+import Banner from '../components/Home/Banner';
+import ProductList from '../components/Home/ProductList';
+
+const data = [
+    { image: require('../assets/images/Banner/Banner1.png') },
+    { image: require('../assets/images/Banner/Banner2.png') },
+    { image: require('../assets/images/Banner/Banner1.png') },
+    { image: require('../assets/images/Banner/Banner2.png') },
+];
 
 export default function Home({ Navigation }) {
     return (
         <View style={styles.Container}>
             <Header Navigation={Navigation} />
+            <Banner Navigation={Navigation} data={data} />
+            <ProductList Navigation={Navigation} />
         </View>
     );
 }
@@ -14,5 +25,6 @@ export default function Home({ Navigation }) {
 const styles = StyleSheet.create({
     Container: {
         flex: 1,
+        backgroundColor: bgWhite,
     },
 });
