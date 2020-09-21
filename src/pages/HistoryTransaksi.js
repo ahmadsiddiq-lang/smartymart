8276dc2192db953596ad7d22e887f7422ae44a4a
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-09-21 16:33:00 +0700

    ravisi

--- src/pages/HistoryTransaksi.js
+++ src/pages/HistoryTransaksi.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { bgWhite } from '../assets/colors';
 import Header from '../components/Headers/HeaderPages';
 import Category from '../components/HistoryTransaksi/Category';
