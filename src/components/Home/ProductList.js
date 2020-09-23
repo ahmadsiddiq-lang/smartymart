0e7c79c789b00bf643eb367bb23d5c3bbe16dc9b
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-09-23 17:29:46 +0700

    revisi home

--- src/components/Home/ProductList.js
+++ src/components/Home/ProductList.js
@@ -1,20 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-import { bgBlack2, fontBlack1, fontWhite, MainColor } from '../../assets/colors';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { bgBlack2, fontBlack1, borderBlack2, MainColor, fontWhite } from '../../assets/colors';
 import { sizeWidth, sizeFont, sizeHeight } from '../../assets/responsive';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const dataProduct = [
-    { image: require('../../assets/images/Product/Produk1.png') },
-    { image: require('../../assets/images/Product/Produk2.png') },
-    { image: require('../../assets/images/Product/Produk3.png') },
-    { image: require('../../assets/images/Product/Produk4.png') },
-    { image: require('../../assets/images/Product/Produk5.png') },
-    { image: require('../../assets/images/Product/Produk6.png') },
-];
+export default function ProductList({ navigation, handleCart, Cart, dataProduct }) {
 
-export default function ProductList({ navigation }) {
+
     return (
         <View style={styles.Container}>
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -28,13 +21,27 @@ export default function ProductList({ navigation }) {
                                 <View style={{ flex: 2, paddingLeft: 15 }}>
                                     <Text style={{ fontSize: sizeFont(3.3) }}>Minyak Goreng Tropical</Text>
                                     <Text style={{ fontSize: sizeFont(3), color: fontBlack1 }}>2 Lt / Pcs</Text>
-                                    <Text style={{ fontSize: sizeFont(3.3) }}>Rp. 24.000</Text>
+                                    <Text style={{ fontSize: sizeFont(3.3) }}>Rp. {item.harga}</Text>
                                 </View>
                                 <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                                    <TouchableOpacity activeOpacity={0.6} style={styles.BtnTambah}>
+                                    <TouchableOpacity onPress={() => handleCart(item)} activeOpacity={0.6} style={styles.BtnTambah}>
                                         <Ionicons name="add" size={sizeFont(5)} color={fontWhite} />
                                         <Text style={{ color: fontWhite, marginLeft: 5 }}>Tambah</Text>
                                     </TouchableOpacity>
+                                    {/* <View style={styles.BoxQty}>
+                                        <TouchableOpacity activeOpacity={0.6}>
+                                            <Ionicons name="remove" color={MainColor} size={sizeFont(4)} style={{ paddingHorizontal: 8 }} />
+                                        </TouchableOpacity>
+                                        <TextInput selectionColor={MainColor} keyboardType="numeric" style={{
+                                            padding: 0,
+                                            minWidth: sizeWidth(5),
+                                            maxWidth: sizeWidth(15),
+                                            textAlign: 'center',
+                                        }} />
+                                        <TouchableOpacity activeOpacity={0.6}>
+                                            <Ionicons name="add" color={MainColor} size={sizeFont(4)} style={{ paddingHorizontal: 8 }} />
+                                        </TouchableOpacity>
+                                    </View> */}
                                 </View>
                             </View>
                         );
@@ -78,5 +85,14 @@ const styles = StyleSheet.create({
         paddingVertical: 3,
         flexDirection: 'row',
         paddingLeft: 10,
+    },
+    BoxQty: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderWidth: 1,
+        alignItems: 'center',
+        borderRadius: 8,
+        borderColor: borderBlack2,
+        marginBottom: 20,
     },
 });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
+import { set } from 'react-native-reanimated';
 import { bgWhite, MainColor, borderWhite, fontWhite } from '../assets/colors';
 import { sizeWidth, sizeFont } from '../assets/responsive';
 import Header from '../components/Headers/HeaderHome';
@@ -14,16 +15,36 @@ const data = [
     { image: require('../assets/images/Banner/Banner2.png') },
 ];
 
+const dataProduct = [
+    { image: require('../assets/images/Product/Produk1.png'), harga: 24000 },
+    { image: require('../assets/images/Product/Produk2.png'), harga: 25000 },
+    { image: require('../assets/images/Product/Produk3.png'), harga: 21000 },
+    { image: require('../assets/images/Product/Produk4.png'), harga: 23000 },
+    { image: require('../assets/images/Product/Produk5.png'), harga: 20000 },
+    { image: require('../assets/images/Product/Produk6.png'), harga: 28000 },
+];
+
 export default function Home({ Navigation }) {
+
+    const [Cart, setCart] = useState([]);
+
+    const handleCart = (dataInput) => {
+        // let newData = [];
+        Cart.push({ ...dataInput });
+        console.log(Cart);
+        setCart(Cart);
+    };
+
+
     return (
         <View style={styles.Container}>
             <Header Navigation={Navigation} />
             <Banner Navigation={Navigation} data={data} />
-            <ProductList Navigation={Navigation} />
+            <ProductList dataProduct={dataProduct} handleCart={handleCart} Cart={Cart} Navigation={Navigation} />
             <View style={styles.Footer}>
                 <TouchableOpacity activeOpacity={0.8} style={styles.Btn}>
                     <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ color: fontWhite, fontSize: sizeFont(3.5) }}>6 Items</Text>
+                        <Text style={{ color: fontWhite, fontSize: sizeFont(3.5) }}>{Cart.length} Items</Text>
                         <View style={{ borderLeftWidth: 1, borderColor: borderWhite, marginLeft: 8, paddingLeft: 8 }}>
                             <Text style={{ color: fontWhite, fontSize: sizeFont(3.5) }}>Rp. 270.000</Text>
                         </View>
